aadd75bd9f121465e9dcd114c6c0047f7e1f71cf
Daniel Ohayon <danielohayon2019@gmail.com>
2025-05-20 21:49:12 +0300

    more data

--- bron_edge_transformers.py
+++ bron_edge_transformers.py
@@ -11,13 +11,20 @@ import requests
 import logging as log
 from singleton import SingletonMeta
 
+
 class EdgeData(metaclass=SingletonMeta):
     def __init__(self):
         attackdata = attackToExcel.get_stix_data('enterprise-attack')
-        self.group_technique_df = stixToDf.groupsToDf(attackdata)['techniques used']
-        self.software_technique_df = stixToDf.softwareToDf(attackdata)['techniques used']
-        self.mitigation_technique_df = stixToDf.mitigationsToDf(attackdata)['techniques addressed']
+        self.group_technique_df = stixToDf.groupsToDf(attackdata)[
+            'techniques used']
+        self.software_technique_df = stixToDf.softwareToDf(attackdata)[
+            'techniques used']
+        self.mitigation_technique_df = stixToDf.mitigationsToDf(attackdata)[
+            'techniques addressed']
         self.cwe_cve_dict = {}
+
+        self.total = {}
+        self.found = {}
 
 
 def TacticTechnique_trans(edge: dict) -> dict:
@@ -31,13 +38,17 @@ def GroupSoftware_trans(edge: dict) -> dict:
 def GroupTechnique_trans(edge: dict) -> dict:
     src_id = edge['_from'].split('/')[-1]
     dst_id = edge['_to'].split('/')[-1]
-    group_technique_df = EdgeData().group_technique_df
-    
+
+    edge_data = EdgeData()
+    group_technique_df = edge_data.group_technique_df
+    edge_data.total['group_technique'] = edge_data.total.get('group_technique', 0) + 1
+
     group_technique = group_technique_df[
-        group_technique_df['source_id'] == src_id & 
-        group_technique_df['target_id'] == dst_id
+        (group_technique_df['source ID'] == src_id) &
+        (group_technique_df['target ID'] == dst_id)
     ]
     if not group_technique.empty:
+        edge_data.found['group_technique'] = edge_data.found.get('group_technique', 0) + 1
         return {'description': group_technique.iloc[0]['mapping description']}
     log.warning(f"Link between group {src_id} and technique {dst_id} not found in MITRE ATT&CK")
     return {}
@@ -46,13 +57,17 @@ def GroupTechnique_trans(edge: dict) -> dict:
 def SoftwareTechnique_trans(edge: dict) -> dict:
     src_id = edge['_from'].split('/')[-1]
     dst_id = edge['_to'].split('/')[-1]
-    software_technique_df = EdgeData().software_technique_df
+
+    edge_data = EdgeData()
+    software_technique_df = edge_data.software_technique_df
+    edge_data.total['software_technique'] = edge_data.total.get('software_technique', 0) + 1
 
     software_technique = software_technique_df[
-        software_technique_df['source_id'] == src_id &
-        software_technique_df['target_id'] == dst_id
+        (software_technique_df['source ID'] == src_id) &
+        (software_technique_df['target ID'] == dst_id)
     ]
     if not software_technique.empty:
+        edge_data.found['software_technique'] = edge_data.found.get('software_technique', 0) + 1
         return {'description': software_technique.iloc[0]['mapping description']}
     log.warning(f"Link between software {src_id} and technique {dst_id} not found in MITRE ATT&CK")
     return {}
@@ -66,28 +81,32 @@ def D3fend_mitigationTechnique_trans(edge: dict) -> dict:
     return {}
 
 
-def Engage_goalEngage_approach(edge: dict) -> dict:
+def Engage_goalEngage_approach_trans(edge: dict) -> dict:
     return {}
 
 
-def Engage_approachEngage_activity(edge: dict) -> dict:
+def Engage_approachEngage_activity_trans(edge: dict) -> dict:
     return {}
 
 
-def TechniqueEngage_activity(edge: dict) -> dict:   
+def TechniqueEngage_activity_trans(edge: dict) -> dict:
     return {}
 
 
 def TechniqueTechnique_mitigation_trans(edge: dict) -> dict:
     technique_id = edge['_from'].split('/')[-1]
     mitigation_id = edge['_to'].split('/')[-1]
-    mitigation_technique_df = EdgeData().mitigation_technique_df
+
+    edge_data = EdgeData()
+    mitigation_technique_df = edge_data.mitigation_technique_df
+    edge_data.total['mitigation_technique'] = edge_data.total.get('mitigation_technique', 0) + 1
 
     mitigation_technique = mitigation_technique_df[
-        mitigation_technique_df['source_id'] == mitigation_id &
-        mitigation_technique_df['target_id'] == technique_id
+        (mitigation_technique_df['source ID'] == mitigation_id) &
+        (mitigation_technique_df['target ID'] == technique_id)
     ]
     if not mitigation_technique.empty:
+        edge_data.found['mitigation_technique'] = edge_data.found.get('mitigation_technique', 0) + 1
         return {'description': mitigation_technique.iloc[0]['mapping description']}
     log.warning(f"Link between mitigation {mitigation_id} and technique {technique_id} not found in MITRE ATT&CK")
     return {}
@@ -132,13 +151,12 @@ def CweCwe_detection_trans(edge: dict) -> dict:
 def CweCve_trans(edge: dict) -> dict:
     cwe_id = edge['_from'].split('/')[-1]
     cve_id = edge['_to'].split('/')[-1]
-    cwe_cve_dict = EdgeData().cwe_cve_dict
 
-    if cwe_id in cwe_cve_dict and cwe_cve_dict[cwe_id] and cve_id in cwe_cve_dict[cwe_id]:
-        return {'description': cwe_cve_dict[cwe_id][cve_id]}
-    elif cwe_id in cwe_cve_dict and cwe_cve_dict[cwe_id] and cve_id not in cwe_cve_dict[cwe_id]:
-        log.warning(f"Connection between CWE {cwe_id} and CVE {cve_id} not found in CWE API")
-    elif cwe_id not in cwe_cve_dict:
+    edge_data = EdgeData()
+    cwe_cve_dict = edge_data.cwe_cve_dict
+    edge_data.total['cwe_cve'] = edge_data.total.get('cwe_cve', 0) + 1
+
+    if cwe_id not in cwe_cve_dict:
         cwe_cve_dict[cwe_id] = {}
         resp = requests.get(f"https://cwe-api.mitre.org/api/v1/cwe/weakness/{cwe_id.split('-')[1]}")
         if resp.status_code == 404:
@@ -148,6 +166,14 @@ def CweCve_trans(edge: dict) -> dict:
             related_cves = resp.json()['Weaknesses'][0].get('ObservedExamples', [])
             for cve_ref in related_cves:
                 cwe_cve_dict[cwe_id][cve_ref['Reference']] = cve_ref['Description']
+
+    if cwe_cve_dict[cwe_id] and cve_id in cwe_cve_dict[cwe_id]:
+        edge_data.found['cwe_cve'] = edge_data.found.get('cwe_cve', 0) + 1
+        return {'description': cwe_cve_dict[cwe_id][cve_id]}
+    elif cwe_cve_dict[cwe_id] and cve_id not in cwe_cve_dict[cwe_id]:
+        log.debug(
+            f"Connection between CWE {cwe_id} and CVE {cve_id} not found in CWE API")
+
     return {}
 
 
